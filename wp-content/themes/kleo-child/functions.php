f02aef97179c8b104e9e59ff8d65d62b6308d525
Brandon Barringer <brandon.barringer@noblestudios.com>
2016-12-10 16:40:53 -0800

    [update] adds plugins and adds styles

--- wp-content/themes/kleo-child/functions.php
+++ wp-content/themes/kleo-child/functions.php
@@ -2,11 +2,23 @@
 /**
  * @package WordPress
  * @subpackage Kleo
- * @author SeventhQueen <themesupport@seventhqueen.com>
- * @since Kleo 1.0
+ * @author Brandon Barringer
+ * @since birdyConnect 1.0
  */
 
-/**
- * Kleo Child Theme Functions
- * Add custom code below
-*/ 
+
+// Pulls in the parent styles and our styles to override where needed
+function birdy_enqueue_styles() {
+
+    $parent_style = 'parent-style'; 
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/assets/styles/application.min.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'birdy_enqueue_styles', '99' );
+
+?>
