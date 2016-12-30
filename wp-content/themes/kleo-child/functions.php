36afbd06124b30377366901910329e154a26ac49
Brandon Barringer <brandon.barringer@noblestudios.com>
2016-12-30 13:43:35 -0800

    [update] cleans up files and structure

--- wp-content/themes/kleo-child/functions.php
+++ wp-content/themes/kleo-child/functions.php
@@ -9,169 +9,22 @@
 
 // Pulls in the parent styles and our styles to override where needed
 function birdy_enqueue_styles() {
-
     $parent_style = 'parent-style'; 
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/assets/styles/application.min.css',
         array( $parent_style ),
         wp_get_theme() -> get( 'Version' )
     );
+
 }
 
 // Creates member industry types for BuddyPress
 function birdy_register_member_types() {
-	$industries = array(
-			'accounting' => 'Accounting',
-			'airlines-aviation' => 'Airlines / Aviation',
-			'alternative-dispute-resolution' => 'Alternative Dispute Resolution',
-			'alternative-medicine' => 'Alternative Medicine',
-			'animation' => 'Animation',
-			'apparel-fashion' => 'Apparel & fashion',
-			'architecture-&-planning' => 'Architecture & Planning',
-			'arts-&-crafts' => 'Arts & Crafts',
-			'automotive' => 'Automotive',
-			'aviation-&-aerospace' => 'Aviation & Aerospace',
-			'banking' => 'Banking',
-			'biotechnology' => 'Biotechnology',
-			'broadcast-media' => 'Broadcast Media',
-			'building-materials' => 'Building Materials',
-			'business-supplies-equipment' => 'Business Supplies & Equipment',
-			'capital-markets' => 'Capital Markets',
-			'chemicals' => 'Chemicals',
-			'civic-&-social-organization' => 'Civic & Social Organization',
-			'civil-engineering' => 'Civil Engineering',
-			'commercial-real-estate' => 'Commercial Real Estate',
-			'computer-&-network-security' => 'Computer & Network Security',
-			'computer-games' => 'Computer Games',
-			'computer-hardware' => 'Computer Hardware',
-			'computer-networking' => 'Computer Networking',
-			'computer-software' => 'Computer Software',
-			'construction' => 'Construction',
-			'consumer-electronics' => 'Consumer Electronics',
-			'consumer-goods' => 'Consumer Goods',
-			'consumer-services' => 'Consumer Services',
-			'cosmetics' => 'Cosmetics',
-			'dairy' => 'Dairy',
-			'defense-space' => 'Defense & Space',
-			'design' => 'Design',
-			'education-management' => 'Education Management',
-			'e-learning' => 'E-Learning',
-			'electrical-&-electronic-manufacturing' => 'Electrical & Electronic Manufacturing',
-			'entertainment' => 'Entertainment',
-			'environmental-services' => 'Environmental Services',
-			'events-services' => 'Events Services',
-			'executive-office' => 'Executive Office',
-			'facilities-services' => 'Facilities Services',
-			'farming' => 'Farming',
-			'financial-services' => 'Financial Services',
-			'fine-art' => 'Fine Art',
-			'fishery' => 'Fishery',
-			'food-beverages' => 'Food & Beverages',
-			'food-production' => 'Food Production',
-			'fundraising' => 'Fundraising',
-			'furniture' => 'Furniture',
-			'gambling-casinos' => 'Gambling & Casinos',
-			'glass-ceramics-concrete' => 'Glass, Ceramics & Concrete',
-			'government-administration' => 'Government Administration',
-			'government-relations' => 'Government Relations',
-			'graphic-design' => 'Graphic Design',
-			'health-wellness-fitness' => 'Health, Wellness & Fitness',
-			'higher-education' => 'Higher Education',
-			'hospital-health-care' => 'Hospital & Health Care',
-			'hospitality' => 'Hospitality',
-			'human-resources' => 'Human Resources',
-			'import-export' => 'Import & Export',
-			'individual-family-services' => 'Individual & Family Services',
-			'industrial-automation' => 'Industrial Automation',
-			'information-services' => 'Information Services',
-			'information-technology-services' => 'Information Technology & Services',
-			'insurance' => 'Insurance',
-			'international-affairs' => 'International Affairs',
-			'international-trade-development' => 'International Trade & Development',
-			'internet' => 'Internet',
-			'investment-banking-venture' => 'Investment Banking / Venture',
-			'investment-management' => 'Investment Management',
-			'judiciary' => 'Judiciary',
-			'law-enforcement' => 'Law Enforcement',
-			'law-practice' => 'Law Practice',
-			'legal-services' => 'Legal Services',
-			'legislative-office' => 'Legislative Office',
-			'leisure-travel' => 'Leisure & Travel',
-			'libraries' => 'Libraries',
-			'logistics-&-supply-chain' => 'Logistics & Supply Chain',
-			'luxury-goods-jewelry' => 'Luxury Goods & Jewelry',
-			'machinery' => 'Machinery',
-			'management-consulting' => 'Management Consulting',
-			'maritime' => 'Maritime',
-			'marketing-advertising' => 'Marketing & Advertising',
-			'market-research' => 'Market Research',
-			'mechanical-industrial-engineering' => 'Mechanical or Industrial Engineering',
-			'media-production' => 'Media Production',
-			'medical-device' => 'Medical Device',
-			'medical-practice' => 'Medical Practice',
-			'mental-health-care' => 'Mental Health Care',
-			'military' => 'Military',
-			'mining-metals' => 'Mining & Metals',
-			'motion-pictures-film' => 'Motion Pictures & Film',
-			'museums-institutions' => 'Museums & Institutions',
-			'music' => 'Music',
-			'nanotechnology' => 'Nanotechnology',
-			'newspapers' => 'Newspapers', 
-			'nonprofit-organization-management' => 'Nonprofit Organization Management',
-			'oil-energy' => 'Oil & Energy',
-			'online-publishing' => 'Online Publishing',
-			'outsourcing-offshoring' => 'Outsourcing / Offshoring',
-			'package-freight-delivery' => 'Package / Freight Delivery',
-			'packaging-containers' => 'Packaging & Containers',
-			'paper-&-forest-products' => 'Paper & Forest Products',
-			'performing-arts' => 'Performing Arts',
-			'pharmaceuticals' => 'Pharmaceuticals',
-			'philanthropy' => 'Philanthropy',
-			'photography' => 'Photography',
-			'plastics' => 'Plastics',
-			'political-organization' => 'Political Organization',
-			'printing' => 'Printing',
-			'professional-training' => 'Professional Training',
-			'program-development' => 'Program Development',
-			'public-policy' => 'Public Policy',
-			'public-relations' => 'Public Relations',
-			'public-safety' => 'Public Safety',
-			'publishing' => 'Publishing',
-			'railroad-manufacture' => 'Railroad Manufacture',
-			'ranching' => 'Ranching',
-			'real-estate' => 'Real Estate',
-			'recreational' => 'Recreational',
-			'facilities-services' => 'Facilities & Services',
-			'religious-institutions' => 'Religious Institutions',
-			'renewables-environment' => 'Renewables & Environment',
-			'research' => 'Research',
-			'restaurants' => 'Restaurants',
-			'retail' => 'Retail',
-			'security-investigations' => 'Security & Investigations',
-			'semiconductors' => 'Semiconductors',
-			'shipbuilding' => 'Shipbuilding',
-			'sporting-goods' => 'Sporting Goods',
-			'sports' => 'Sports',
-			'staffing-&-recruiting' => 'Staffing & Recruiting',
-			'supermarkets' => 'Supermarkets',
-			'technology' => 'Technology',
-			'telecommunications' => 'Telecommunications',
-			'textiles' => 'Textiles',
-			'think-tanks' => 'Think Tanks',
-			'tobacco' => 'Tobacco',
-			'translation-&-localization' => 'Translation & Localization',
-			'transportation-trucking-railroad' => 'Transportation / Trucking / Railroad',
-			'utilities' => 'Utilities',
-			'venture-capital' => 'Venture Capital',
-			'veterinary' => 'Veterinary',
-			'warehousing' => 'Warehousing',
-			'wholesale' => 'Wholesale',
-			'wine-spirits' => 'Wine & Spirits',
-			'wireless' => 'Wireless',
-			'writing-&-editing' => 'Writing & Editing'
-		);
+
+	include_once('inc/industries.php'); // array of industry types; slug => string
 
 	foreach ( $industries as $industry => $str_name ) {
 		bp_register_member_type( $industry, array(
@@ -179,9 +32,12 @@ function birdy_register_member_types() {
 			'has_directory' => true
 			));
 	}
+
 }
+// register memeber types in buddyPress
 add_action( 'bp_register_member_types', 'birdy_register_member_types' );
 
+// enqueues styles in last position
 add_action( 'wp_enqueue_scripts', 'birdy_enqueue_styles', '99' );
 
 ?>
