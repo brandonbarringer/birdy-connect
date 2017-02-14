1b9af8c89e15a95f4d97b81d138d81f9d4ed6b33
Nitish Narala <nitish.narala@noblestudios.com>
2017-02-13 22:34:59 -0800

    Add some footer bar styling

--- wp-content/themes/kleo-child/page-parts/socket.php
+++ wp-content/themes/kleo-child/page-parts/socket.php
@@ -3,6 +3,11 @@
 		================================================ -->
 
 		<div id="socket" class="socket-color footer-bar">
+			<div class="footer-graphic" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/footer.jpg' ?>');">
+				<img class="logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/svgs/birdyConnect_Logo-White.svg'; ?>">
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet faucibus elit, tincidunt viverra est.  </p>
+				<a href="#" class="b-button"> Sign Up </a>
+			</div>
 			<div class="container">
 				<div class="template-page tpl-no col-xs-12 col-sm-12">
 					<div class="wrap-content">
