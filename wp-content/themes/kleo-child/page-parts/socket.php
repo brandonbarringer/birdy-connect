64030166a745110f7d0141e3e9d2c6780c3edc35
Nitish Narala <nitish.narala@noblestudios.com>
2017-03-27 21:18:59 -0700

    Add sign up links

--- wp-content/themes/kleo-child/page-parts/socket.php
+++ wp-content/themes/kleo-child/page-parts/socket.php
@@ -5,8 +5,10 @@
 		<div id="socket" class="socket-color footer-bar">
 			<div class="footer-graphic" style="background-image: url('<?php echo get_stylesheet_directory_uri() . '/assets/images/footer.jpg' ?>');">
 				<img class="logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/svgs/birdyConnect_Logo-White.svg'; ?>">
-				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet faucibus elit, tincidunt viverra est.  </p>
-				<a href="#" class="b-button"> Sign Up </a>
+					<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet faucibus elit, tincidunt viverra est.  </p>
+				<?php if( !is_user_logged_in() ): ?>
+					<a href="/register" class="b-button"> Sign Up </a>
+				<?php endif; ?>
 			</div>
 			<div class="container">
 				<div class="template-page tpl-no col-xs-12 col-sm-12">
